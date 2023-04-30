c976579428e6906a5440c40d75b72d7d2add5f39
Marcio <marciogarrido@id.uff.br>
2023-04-29 23:13:53 -0300

    alterado tamanho campo local do sensor

--- Device/models.py
+++ Device/models.py
@@ -11,7 +11,7 @@ class Tipo(models.Model):
 class Sensor(models.Model):
     sensor = models.CharField(max_length=30,blank=False,verbose_name = 'Nome do sensor')
     tipo = models.ForeignKey(Tipo,blank=False, on_delete=models.CASCADE)
-    local = models.CharField(max_length=11 ,blank=False, null= False, verbose_name = 'Local de Instalação')
+    local = models.CharField(max_length=30 ,blank=False, null= False, verbose_name = 'Local de Instalação')
     macaddress = models.CharField(max_length=11 ,null=False,verbose_name = 'Endereço MAC', unique=True)
     observacao = models.TextField(max_length=100 ,blank=False, null= True, verbose_name = 'Observação')
     data_cadastro = models.DateField()
