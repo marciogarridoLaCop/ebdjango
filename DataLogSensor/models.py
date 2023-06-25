c6662ead09aacb63741548990810e4ce55a65a12
Marcio <marciogarrido@id.uff.br>
2023-06-25 11:04:07 -0300

    subindo banco sem restricoes de campos obrigatórios

--- DataLogSensor/models.py
+++ DataLogSensor/models.py
@@ -5,29 +5,29 @@ from datetime import datetime
 
 class Registro(models.Model):
 
-    estacao = models.ForeignKey(Sensor, blank=False, null=False,on_delete=models.CASCADE,verbose_name = 'Identificação da Estação')
-    data= models.CharField(max_length=10,blank=True, null=False,verbose_name = 'Data')
-    hora= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Hora')
-    temp_min= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Temperatura Mínima')
-    temp_max= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Temperatura Máxima')
-    temp_media= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Temperatura Média')
+    estacao = models.ForeignKey(Sensor, blank=False, null=True,on_delete=models.CASCADE,verbose_name = 'Identificação da Estação')
+    data= models.CharField(max_length=10,blank=True, null=True,verbose_name = 'Data')
+    hora= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Hora')
+    temp_min= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Temperatura Mínima')
+    temp_max= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Temperatura Máxima')
+    temp_media= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Temperatura Média')
     
-    umidade_min= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Umidade Mínima')
-    umidade_max= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Umidade Máxima')
-    umidade_media= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Umidade Média')
+    umidade_min= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Umidade Mínima')
+    umidade_max= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Umidade Máxima')
+    umidade_media= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Umidade Média')
 
-    vm_min= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Velocidade Mínima')
-    vm_max= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Velocidade Máxima')
-    vm_media= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Velocidade Média')
+    vm_min= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Velocidade Mínima')
+    vm_max= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Velocidade Máxima')
+    vm_media= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Velocidade Média')
    
 
-    pressao_min= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Pressão Mínima')
-    pressao_max= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Pressão Máxima')
-    pressao_media= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Pressao Média')
+    pressao_min= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Pressão Mínima')
+    pressao_max= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Pressão Máxima')
+    pressao_media= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Pressao Média')
 
-    direcao= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'Direção do vento')
-    w_m2= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'W/m2')
-    uv= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'UV')
+    direcao= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'Direção do vento')
+    w_m2= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'W/m2')
+    uv= models.CharField(max_length=8,blank=True, null=True,verbose_name = 'UV')
     mm_ciclo= models.CharField(max_length=8,blank=True, null=False,verbose_name = 'mm/ciclo')
     data_registro = models.DateTimeField(auto_now=True,verbose_name='Data do registro') 
   
